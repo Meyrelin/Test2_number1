28bb14a3cff8867ba20fcbc76f742382375a00df
Meyrelin <42929360+Meyrelin@users.noreply.github.com>
2018-09-23 19:07:54 +0300

    Initial commit

---
+++ src/power_2.java
@@ -0,0 +1,13 @@
+public class power_2
+{
+public static void main (String [] args)
+{
+    int a = 2;
+int i;
+   {
+      for (i = 0; i <= 10; i++) {
+         System.out.printf("Result = %.0f\n", Math.pow(a, i));
+      }
+   }
+}
+}
